55dd56e10e0fd742655940f86219bb2cf6d24499
Rexben001 <rexben.rb@gmail.com>
2019-11-15 09:48:27 +0100

    add the cpu methods

--- cpu.py
+++ cpu.py
@@ -1,3 +1,7 @@
+"""CPU functionality."""
+
+import sys
+
 LDI = 0b10000010
 PRN = 0b01000111
 HLT = 0b00000001
@@ -19,6 +23,10 @@ NOT = 0b01101001
 SHL = 0b10101100
 SHR = 0b10101101
 MOD = 0b10100100
+
+
+class CPU:
+    """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
@@ -50,3 +58,186 @@ MOD = 0b10100100
         self.branchtable[SHL] = self.SHL
         self.branchtable[SHR] = self.SHR
         self.branchtable[MOD] = self.MOD
+
+    """ START ALU function calls"""
+
+    def ldi(self, operand_a, operand_b):
+        self.reg[operand_a] = operand_b
+        self.pc += 3
+
+    def prn(self, operand_a, operand_b):
+        print(self.reg[operand_a])
+        self.pc += 2
+
+    def hlt(self, operand_a, operand_b):
+        sys.exit(1)
+
+    def mul(self, operand_a, operand_b):
+        self.alu("MUL", operand_a, operand_b)
+        self.pc += 3
+
+    def add(self, operand_a, operand_b):
+        self.alu("ADD", operand_a, operand_b)
+        self.pc += 3
+
+    def AND(self, operand_a, operand_b):
+        self.alu("AND", operand_a, operand_b)
+        self.pc += 3
+
+    def OR(self, operand_a, operand_b):
+        self.alu("AND", operand_a, operand_b)
+        self.pc += 3
+
+    def XOR(self, operand_a, operand_b):
+        self.alu("XOR", operand_a, operand_b)
+        self.pc += 3
+
+    def NOT(self, operand_a, operand_b):
+        self.alu("NOT", operand_a, operand_b)
+        self.pc += 2
+
+    def SHL(self, operand_a, operand_b):
+        self.alu("SHL", operand_a, operand_b)
+        self.pc += 3
+
+    def SHR(self, operand_a, operand_b):
+        self.alu("SHR", operand_a, operand_b)
+        self.pc += 3
+
+    def MOD(self, operand_a, operand_b):
+        self.alu("MOD", operand_a, operand_b)
+        self.pc += 3
+
+    def cmp(self, operand_a, operand_b):
+        self.alu("CMP", operand_a, operand_b)
+        self.pc += 3
+
+    def pop(self, operand_a, operand_b):
+        value = self.ram[self.sp]
+        self.reg[operand_a] = value
+        self.pc += 2
+
+    def push(self, operand_a, operand_b):
+        self.sp -= 1
+        value = self.reg[operand_a]
+        self.ram_write(self.sp, value)
+        self.pc += 2
+
+    def call(self, operand_a, operand_b):
+        self.sp -= 1
+        value = self.pc + 2
+        self.ram_write(self.sp, value)
+        self.pc = self.reg[operand_a]
+
+    def ret(self, operand_a, operand_b):
+        value = self.ram[self.sp]
+        self.pc = value
+
+    def jmp(self, operand_a, operand_b):
+        self.pc = self.reg[operand_a]
+
+    def jeq(self, operand_a, operand_b):
+        if self.flags == 0b00000001:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
+
+    def jne(self, operand_a, operand_b):
+        if self.flags != 0b00000001:
+            self.pc = self.reg[operand_a]
+        else:
+            self.pc += 2
+
+    def addi(self, operand_a, operand_b):
+        self.reg[operand_a] += operand_b
+        self.pc += 3
+
+    def load(self, filename):
+        try:
+            address = 0
+            with open(filename) as f:
+                for line in f:
+                    comment_split = line.split("#")
+                    num = comment_split[0].strip()
+                    if len(num) == 0:
+                        continue
+                    value = int(num, 2)
+                    self.ram[address] = value
+                    address += 1
+        except FileNotFoundError:
+            print(f"{sys.argv[0]}: {sys.argv[1]} not found")
+            sys.exit(2)
+
+    def alu(self, op, reg_a, reg_b):
+        """ALU operations."""
+
+        if op == "ADD":
+            self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "CMP":
+            if self.reg[reg_a] > self.reg[reg_b]:
+                self.flags = 0b00000010
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                self.flags = 0b00000100
+            else:
+                self.flags = 0b00000001
+        elif op == 'AND':
+            self.reg[reg_a] = self.reg[reg_a] and self.reg[reg_b]
+        elif op == 'OR':
+            self.reg[reg_a] = self.reg[reg_a] or self.reg[reg_b]
+        elif op == 'XOR':
+            self.reg[reg_a] = self.reg[reg_a] ^ self.reg[reg_b]
+        elif op == 'NOT':
+            self.reg[reg_a] = ~ (self.reg[reg_a])
+        elif op == 'SHL':
+            self.reg[reg_a] = self.reg[reg_a] << self.reg[reg_b]
+        elif op == 'SHR':
+            self.reg[reg_a] = self.reg[reg_a] >> self.reg[reg_b]
+        elif op == 'MOD':
+            if self.reg[reg_b]:
+                self.reg[reg_a] = self.reg[reg_a] % self.reg[reg_b]
+            else:
+                print(f"{reg_b} is 0")
+                self.hlt(reg_a, reg_b)
+        else:
+            raise Exception("Unsupported ALU operation")
+
+    def trace(self):
+        """
+        Handy function to print out the CPU state. You might want to call this
+        from run() if you need help debugging.
+        """
+
+        print(f"TRACE: %02X | %02X %02X %02X |" % (
+            self.pc,
+            # self.fl,
+            # self.ie,
+            self.ram_read(self.pc),
+            self.ram_read(self.pc + 1),
+            self.ram_read(self.pc + 2)
+        ), end='')
+
+        for i in range(8):
+            print(" %02X" % self.reg[i], end='')
+
+        print()
+
+    def ram_read(self, address):
+        return self.ram[address]
+
+    def ram_write(self, address, value):
+        self.ram[address] = value
+
+    def run(self):
+        """Run the CPU."""
+
+        while True:
+            IR = self.ram_read(self.pc)
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+
+            if IR not in self.branchtable:
+                sys.exit(1)
+            else:
+                self.branchtable[IR](operand_a, operand_b)
